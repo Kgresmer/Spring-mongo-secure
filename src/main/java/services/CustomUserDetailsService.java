31d93f13a6049413618c4581f9eb1e6bcaa3ff05
kgresmer <kgresmer@gmail.com>
2018-06-27 05:56:55 -0500

    Added in domain classes and the start of a repo

---
+++ src/main/java/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package services;
+
+import domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import repository.RoleRepository;
+import repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+    }
+}
